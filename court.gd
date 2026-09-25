19d098ec66276779473679a18ee0fb108436750e
Benji42 <cheezyanimations@gmail.com>
2026-09-25 17:08:31 -0400

    bruh

--- court.gd
+++ court.gd
@@ -7,3 +7,7 @@ func _on_player_3d_create_fx(fx: PackedScene, pos: Vector3) -> void:
 func _on_player_3d_on_hit_ball() -> void:
 	%FxManager.create_with_parent_3D(FXManager.pressure_ring, %Ball3d)
 	%Camera3D.add_trauma(.1)
+
+
+func _on_ball_3d_create_fx(fx: PackedScene, pos: Vector3) -> void:
+	%FxManager.create_at_pos(fx, pos)

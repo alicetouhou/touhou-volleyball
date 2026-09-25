15d57872ea8a9dbb3d0d81db36bf783807c60ea0
Zunda <mailme@jacobhaussman.online>
2026-09-25 13:14:49 -0400

    turning animation

--- game_objects/player_3d.gd
+++ game_objects/player_3d.gd
@@ -8,12 +8,21 @@ const UP_KICK_POWER = Vector2()
 const FORWARD_KICK_POWER = Vector2()
 const DOWN_KICK_POWER = Vector2()
 
+const KICK_TIME_ALLOW = .15
+var time_since_kick_pressed = 1000
+
+@onready var Animations = %AnimationTree.get("parameters/playback")
+
 func kick():
+	Animations.travel("kick_miss")
 	var bodies = %KickHitbox.get_overlapping_bodies()
 	var ball_index = bodies.find_custom(func(x): return x.is_in_group("ball"))
 
 	if ball_index < 0:
 		return
+
+	Animations.travel("kick_hit")
+	time_since_kick_pressed = 10000
 
 	var ball = bodies[ball_index]
 	
@@ -39,16 +48,25 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
-	
+
 	apply_central_force(Vector3(x, 0, 0)  * MOVEMENT_SPEED)
 	
 	if sign(x) != direction and x != 0:
 		direction = sign(x)
+		
+		if direction < 0:
+			Animations.travel("turn")
+			%Turning.play("turn_left")
+		if direction > 0:
+			Animations.travel("turn")
+			%Turning.play("turn_right")
 
 	if Input.is_action_just_pressed("up"):
 		apply_central_impulse(Vector3(0, JUMP_POWER, 0))
 
-
-func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("kick"):
+		time_since_kick_pressed = 0
+
+	time_since_kick_pressed += delta
+	if time_since_kick_pressed < KICK_TIME_ALLOW:
 		kick()

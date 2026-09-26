e87093c8898e3a83794218216c4f1c021ed150b1
Zunda <mailme@jacobhaussman.online>
2026-09-25 22:25:31 -0400

    fix stuff

--- court.gd
+++ court.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-var current_player = 1
+var players_turn = 1
 var round_playing = false
 
 func _ready() -> void:
@@ -20,33 +20,38 @@ func _on_ball_3d_create_fx(fx: PackedScene, pos: Vector3) -> void:
 
 func start_round():
 	%Ball3d.linear_velocity = Vector3.ZERO
+	%Player1.linear_velocity = Vector3.ZERO
+	%Player2.linear_velocity = Vector3.ZERO
 	%Ball3d.process_mode = Node.PROCESS_MODE_DISABLED
 
-	%Ball3d.position = %P1BallSpawnPoint.position
+	if players_turn == 1:
+		%Ball3d.position = %P1BallSpawnPoint.position
+	if players_turn == 2:
+		%Ball3d.position = %P2BallSpawnPoint.position
+	
 	%Player1.position = %P1SpawnPoint.position
 	%Player2.position = %P2SpawnPoint.position
-	
-	%CountdownTimer.show()
 
-	%CountdownTimer.text = "3"
-	await get_tree().create_timer(.75).timeout
-	%CountdownTimer.text = "2"
-	await get_tree().create_timer(.75).timeout
-	%CountdownTimer.text = "1"
-	await get_tree().create_timer(.75).timeout
+	await get_tree().create_timer(1).timeout
 
-	%CountdownTimer.hide()
 	round_playing = true
 
 	%Ball3d.process_mode = Node.PROCESS_MODE_INHERIT
 
 func end_round():
+	if %Ball3d.position.x < 0:
+		players_turn = 1
+	if %Ball3d.position.x >= 0:
+		players_turn = 2
+
 	if not round_playing:
 		return
+
 	round_playing = false
-	%CountdownTimer.text = "Score!"
+	%Score.show()
 
 	await get_tree().create_timer(.75).timeout
+	%Score.hide()
 
 	start_round()
 
